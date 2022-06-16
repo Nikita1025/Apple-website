f0559898414ae675141d4dfd53ce8e4eec6c7bc3
Nikita1025 <nekit1025@mail.ru>
2022-06-16 21:35:13 +0300

    initial commit

---
+++ config/plugins/clearTerminal.plugin.js
@@ -0,0 +1,8 @@
+const TerminalClearPlugin = require('../modules/terminal-clear-plugin')
+
+
+/**
+ * Clears the terminal screen.
+ * ../modules/clear-termina-plugin
+ */
+module.exports = new TerminalClearPlugin({  })
---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+import audio from '@/audio/click.mp3'
+import Cursor from './libs/Cursor'
+import greeting from './libs/greeting'
+
+
+/* The initial HTML document has been completely loaded and parsed */
+document.addEventListener('DOMContentLoaded', () => {
+
+})
+
+
+/**
+ * The whole page has been completely loaded, including
+ * all dependent resources (stylesheets and images)
+ */
+window.addEventListener('load', () => {
+  /** Greetings, Sire! */
+  greeting({ name: 'janeRivas', email: 'solovyev.a@icloud.com' })
+
+  /** Cusrom cursor */
+  new Cursor({ audio })
+})
